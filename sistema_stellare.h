bc0e3c9f49bd85afedfa8dd1b2abb668b03e1278
Vitolus <vito.davide00@gmail.com>
2021-08-04 18:26:43 +0200

    ultimate Sistema_solare

--- sistema_stellare.h
+++ sistema_stellare.h
@@ -5,10 +5,95 @@
 
 template <class T>
 class Sistema_stellare{
-    int length;
-    int dim;
-    T *ele;
+    typedef std::size_t size_type;
+    typedef T value_type;
+    typedef T& reference;
+    typedef T* pointer;
+    typedef std::ptrdiff_t difference_type;
+
+    size_type length;
+    size_type dim;
+    pointer ele;
 public:
+
+    class Iterator{
+        typedef T value_type;
+        typedef T& reference;
+        typedef T* pointer;
+        typedef std::forward_iterator_tag iterator_category;
+        typedef std::ptrdiff_t difference_type;
+
+        T* ptr;
+    public:
+        Iterator(pointer p) : ptr(p){}
+
+        reference operator*() const{
+            return *ptr;
+        }
+
+        pointer operator->(){
+            return ptr;
+        }
+
+        Iterator &operator++(){
+            ptr++;
+            return *this;
+        }
+
+        Iterator operator++(int){
+            Iterator temp= *this;
+            ++(*this);
+            return temp;
+        }
+
+        bool operator==(const Iterator& it){
+            return ptr == it.ptr;
+        }
+
+        bool operator!=(const Iterator& it){
+            return ptr != it.ptr;
+        }
+    };
+
+    class Const_iterator{
+        typedef T value_type;
+        typedef T& reference;
+        typedef T* pointer;
+        typedef std::forward_iterator_tag iterator_category;
+        typedef std::ptrdiff_t difference_type;
+
+        T* ptr;
+    public:
+        Const_iterator(pointer p) : ptr(p){}
+
+        const value_type& operator*() const{
+            return *ptr;
+        }
+
+        const value_type* operator->(){
+            return ptr;
+        }
+
+        Iterator &operator++(){
+            ptr++;
+            return *this;
+        }
+
+        Iterator operator++(int){
+            Iterator temp= *this;
+            ++(*this);
+            return temp;
+        }
+
+        bool operator==(const Iterator& it){
+            return ptr == it.ptr_;
+        }
+
+        bool operator!=(const Iterator& it){
+            return ptr != it.ptr_;
+        }
+    };
+
 
     Sistema_stellare() : length(), ele(nullptr){}
 
@@ -16,7 +101,7 @@ public:
      * @brief costruisce Sistema_stellare vuoto con lunghezza l
      * @param l lunghezza
      */
-    explicit Sistema_stellare(int l) : length(l){
+    explicit Sistema_stellare(size_type l) : length(l){
         assert(length>=0);
         if(l>0) ele= new T[length];
         dim= 0;
@@ -36,7 +121,7 @@ public:
     /**
      * @brief move constructor
      * @param s Sistema_stellare da rimpiazzare
-     */
+     *//*
     Sistema_stellare(Sistema_stellare &&s){
         length= s.length;
         dim= length;
@@ -44,7 +129,7 @@ public:
         s.ele= nullptr;
         s.length= 0;
         s.dim= 0;
-    }
+    }*/
 
     ~Sistema_stellare(){
         if(ele) delete [] ele;
@@ -67,7 +152,7 @@ public:
      * @brief move assignment operator
      * @param s Sistema_solare da assegnare
      * @return nuovo Sistema_solare (*this)
-     */
+     *//*
     Sistema_stellare &operator=(Sistema_stellare &&s){
         erase();
         length= s.length;
@@ -76,19 +161,19 @@ public:
         s.ele= nullptr;
         s.length= 0;
         s.dim= 0;
-    }
+    }*/
 
     /**
      * @brief accede all'oggetto specificato con controllo limiti
      * @param i posizione dell'elemento
      * @return referenza elemento richiesto
      */
-    T &at(int i){
+    reference at(size_type i){
         assert(i>=0 && i < dim);
         return ele[i];
     }
 
-    T &operator[](T i){
+    reference operator[](value_type i){
         assert(i>=0 && i < dim);
         return ele[i];
     }
@@ -97,7 +182,7 @@ public:
      * @brief accede al primo elemento
      * @return referenza primo elemento
      */
-    T &front(){
+    reference front(){
         return ele[0];
     }
 
@@ -105,7 +190,7 @@ public:
      * @brief accede all'ultimo elemento
      * @return referenza ultimo elemento
      */
-    T &back(){
+    reference back(){
         return ele[dim-1];
     }
 
@@ -113,7 +198,7 @@ public:
      * @brief accesso diretto all'array sottostante
      * @return puntatore primo elemento
      */
-    T *data() noexcept{
+    pointer data() noexcept{
         return ele;
     }
 
@@ -121,7 +206,7 @@ public:
      * @brief accesso diretto all'array sottostante costantemente
      * @return puntatore al primo elemento
      */
-    const T *data() const noexcept{
+    const pointer data() const noexcept{
         return ele;
     }
 
@@ -129,81 +214,84 @@ public:
       * @brief ritorna un iteratore all'inizio dell'array
       * @return iteratore al primo elemento
       */
-     QIterator<T> begin() noexcept{
-        return QIterator(ele);
+    Iterator begin() noexcept{
+        return Iterator(&ele);
     }
 
-     /**
-      * @brief ritorna un iteratore costante all'inizio dell'array
-      * @return iteratore costante al primo elemento
-      */
-     QConstIterator<T> begin() const noexcept{
-         return QConstIterator(ele);
-     }
-
-     /**
+    /**
       * @brief ritorna un iteratore alla fine dell'array
       * @return iteratore all'elemento successivo all'ultimo
       */
-     QIterator<T> end() noexcept{
-         return QIterator(ele[dim]);
-     }
+    Iterator end() noexcept{
+        return Iterator(&ele[dim]);
+    }
 
-     /**
-      * @brief ritorna un iteratore costante alla fine dell'array
-      * @return iteratore costante all'elemento successivo all'ultimo
-      */
-     QConstIterator<T> end() const noexcept{
-         return QConstIterator(ele[dim]);
-     }
-     /**
+    /**
       * @brief controlla se Sistema_stellare vuoto
       * @return vero se vuoto, falso altrimenti
       */
-     bool empty() const noexcept{
-         return dim==0;
-     }
+    bool empty() const noexcept{
+        return dim==0;
+    }
 
-     /**
+    /**
       * @brief ritorna dimensione occupata
       * @return dim
       */
-     int size() const noexcept {
-         return dim;
-     }
+    size_type size() const noexcept {
+        return dim;
+    }
 
-     /**
+    /**
       * @brief ritorna dimensione massima
       * @return length
       */
-     int capacity() const noexcept{
-         return length;
-     }
+    size_type capacity() const noexcept{
+        return length;
+    }
 
-     /**
+    /**
       * @brief cancella gli elementi del Sistema_stellare lasciando invariata la dimensione massima
       */
-     void clear() noexcept{
-         delete [] ele;
-         ele= new T[length];
-         dim= 0;
-     }
+    void clear() noexcept{
+        delete [] ele;
+        ele= new T[length];
+        dim= 0;
+    }
 
-     /**
+    /**
      * @brief elimina elemento in posizione pos
      * @param indice di posizione
      */
-    void erase(int pos){
-        assert(pos>=0 && pos < dim);
-        delete ele[pos];
+    Iterator erase(Iterator pos){
+        assert(pos>=ele && pos < ele[dim]);
+        Iterator temp= pos+1;
+        delete pos;
         dim= dim-1;
+        return temp;
     }
+
+    /**
+     * @brief aggiunge in coda elemento v
+     * @param elemento da aggiungere
+     */
+    void push_back(const value_type& v){
+        if(dim==length) reserve(2*length+1);
+        ele[dim++]= v;
+    }
+
+    /**
+     * @brief elimina ultimo elemento di Sistema_solare
+     */
+    void pop_back(){
+        delete ele[dim--];
+    };
 
     /**
      * @brief modifica la dimensione massima del Sistema_stellare
      * @param nuova dimensione massima
      */
-    void resize(int newLength){
+    void reserve(int newLength){
         if(length==newLength) return;
         length= newLength;
         T* data= new T[length];
@@ -212,6 +300,8 @@ public:
         delete [] ele;
         ele= data;
     }
+
+
 };
 
 #endif // SISTEMA_STELLARE_H
